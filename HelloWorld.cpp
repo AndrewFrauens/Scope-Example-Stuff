a1d26c4907ee94e80fbf6a6110580c70e1c3b318
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-05-06 01:55:51 -0500

    added hello world cpp example

---
+++ HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //required to output/input from terminal
+
+int main() { //cpp requires a main function to know where to start
+    std::cout << "Hello World!" << std::endl; //feed the string "Hello world! to the output stream and then end the line
+    return 0; //return 0 from main indicates that the program ran successfully.
+}
